0d0063d20d1f1d469307b51afa021f11101143dc
fangda <fangda@outlook.com>
2016-04-10 12:11:58 +0800

    添加student.cs

---
+++ BusinessLayer/Entities/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessLayer.Entities
+{
+    public class Student : EntityBase
+    {
+        public string Name { get; set; }
+        
+    }
+}
