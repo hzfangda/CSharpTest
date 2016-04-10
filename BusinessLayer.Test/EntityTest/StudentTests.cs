96813222bb6e1bae590e0ebc809367c7e84ce420
fangda <fangda@outlook.com>
2016-04-10 12:20:32 +0800

    创建单元测试项目

---
+++ BusinessLayer.Test/EntityTest/StudentTests.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Entities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessLayer.Test.EntityTest
+{
+    [TestFixture]
+    public class StudentTests
+    {
+        [Test]
+        public void Add()
+        {
+            var student = new Student();
+
+            int a = 10;
+            int b = 20;
+            Assert.AreEqual(30, student.Add(a, b));
+        }
+    }
+}
--- BusinessLayer/Entities/Student.cs
+++ BusinessLayer/Entities/Student.cs
@@ -8,6 +8,10 @@ namespace BusinessLayer.Entities
     public class Student : EntityBase
     {
         public string Name { get; set; }
-        
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
     }
 }
